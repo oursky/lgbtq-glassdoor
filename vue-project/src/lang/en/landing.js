c5ecc516cf1528b0b9a461d590e54d8bf56812b4
david90 <david.ng@oursky.com>
2018-07-27 17:56:43 +0800

    Update copywriting in top right text #18

--- vue-project/src/lang/en/landing.js
+++ vue-project/src/lang/en/landing.js
@@ -4,7 +4,7 @@ module.exports = {
   splash_text: 'LGBTQ Glassdoor aims to help LGBTQ individuals share and find supportive work environments. LGBTQ individuals & allies can add inclusive companies even if they may not yet have official policies.',
   view_company_text: 'View Company by Tags',
   lets_do_it_together: 'Let\'s do it together!',
-  lets_do_it_together_text: 'Our goal is to crowdsource information on LGBTQ-friendly companies and workspaces for Hong Kong.\n\nAnyone can add to the list and it is publicly available to view.',
+  lets_do_it_together_text: 'Our goal is to crowdsource a directory of LGBTQ-friendly companies and workspaces for Hong Kong through individual contributions.\n\nWe welcome LGBTQ-identified individuals and allies to make anonymous recommendations for companies they would recommend to an LGBTQ person.',
   add_company: 'Add company',
   search: 'Search',
   all_listings: 'All listings'
--- vue-project/src/lang/zh-cn/landing.js
+++ vue-project/src/lang/zh-cn/landing.js
@@ -4,7 +4,7 @@ module.exports = {
   splash_text: 'LGBTQ Glassdoor是一個多元性別友善企業的列表，旨在推動多元性別友善的職場環境和幫助LGBTQ人士發掘友善的工作環境。我們歡迎LGBTQ人士和盟友們將,他們認識的多元性別友善企業加到這個列表去，與其他的LGBTQ人士分享。你所推薦的公司不一定要有多元性別友善的官方政策，更重要的是你與這間公司互動時的感受和經歷！',
   view_company_text: '按標籤查看公司',
   lets_do_it_together: '讓我們一起做吧！',
-  lets_do_it_together_text: '我們的目標是為香港提供有關LGBTQ友好公司和工作場所的信息。 任何人都可以添加，並且可以公開查看。',
+  lets_do_it_together_text: '我們的目標是透過 Crowdsource 模式，為香港提供一個 LGBTQ友好的公司和工作場所列表。\n\n歡迎各位 LGBTQ 朋友和盟友，透過這個列表向LGBTQ人士推薦友善的工作環境。請放心，所有提交的個人資料也是匿名的。',
   add_company: '添加',
   search: '搜尋',
   all_listings: '所有公司'
