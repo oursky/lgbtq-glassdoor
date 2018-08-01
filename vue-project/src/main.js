0ac182bc44c6931a8d7bb8a7e0bdcfa2ae7585a4
david90 <david.ng@oursky.com>
2018-08-01 12:57:11 +0800

    Implement basic SEO wmeta tags , ogtags and twitter card

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -33,18 +33,130 @@ Vue.use(Autocomplete)
 Vue.config.productionTip = false
 
 const routes = [
-  { name: 'search', path: '/search', component: CompanyListPage, props: { search: true, searchText: '' } },
-  { name: 'searchWithText', path: '/search/:searchText', component: CompanyListPage, props: { search: true } },
-  { name: 'list', path: '/list', component: CompanyListPage, props: { searchText: '' } },
-  { name: 'tag', path: '/tag/:tag', component: CompanyListPage, props: { searchText: 'tag:' } },
-  { name: 'detail', path: '/detail/:companyName', component: CompanyDetailPage, props: true },
-  { name: 'add', path: '/add', component: AddCompanyPage },
-  { name: 'subscribe', path: '/subscribe', component: AddCompanyPage },
-  { name: 'home', path: '/', component: LandingPage }
+  { name: 'search',
+    path: '/search',
+    component: CompanyListPage,
+    props: {
+      search: true,
+      searchText: ''
+    },
+    meta: {
+      metaTags: [
+        {
+          property: 'og:url',
+          content: 'https://lgbtqglassdoor.com/#search'
+        }
+      ]
+    }
+  },
+  { name: 'searchWithText',
+    path: '/search/:searchText',
+    component: CompanyListPage,
+    props: {
+      search: true
+    }
+  },
+  { name: 'tag',
+    path: '/tag/:cat/:tag',
+    component: CompanyListPage,
+    props: true
+  },
+  { name: 'list',
+    path: '/list',
+    component: CompanyListPage,
+    props: { searchText: '' },
+    meta: {
+      metaTags: [
+        {
+          property: 'og:url',
+          content: 'https://lgbtqglassdoor.com/#list/'
+        }
+      ]
+    }
+  },
+  { name: 'detail',
+    path: '/detail/:companyName',
+    component: CompanyDetailPage,
+    props: true,
+    meta: {
+      metaTags: [
+        {
+          property: 'og:url',
+          content: 'https://lgbtqglassdoor.com/#detail/'
+        }
+      ]
+    }
+  },
+  { name: 'add',
+    path: '/add',
+    component: AddCompanyPage,
+    meta: {
+      metaTags: [
+        {
+          property: 'og:url',
+          content: 'https://lgbtqglassdoor.com/#add'
+        }
+      ]
+    }
+  },
+  { name: 'subscribe',
+    path: '/subscribe',
+    component: AddCompanyPage
+  },
+  { name: 'home',
+    path: '/',
+    component: LandingPage,
+    meta: {
+      metaTags: [
+        {
+          property: 'og:url',
+          content: 'https://lgbtqglassdoor.com/'
+        }
+      ]
+    }
+  }
 ]
 
 const router = new VueRouter({
   routes // short for `routes: routes`
+})
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+  // const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el))
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next()
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta')
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+
+    // We use this to track which meta tags we create, so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '')
+
+    return tag
+  })
+  // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag))
+
+  next()
 })
 
 /* eslint-disable no-new */
