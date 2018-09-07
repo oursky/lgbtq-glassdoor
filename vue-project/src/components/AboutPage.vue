4a76f6ae9e1a02d23f2c592db2299c7db7757779
david90 <david.ng@oursky.com>
2018-09-07 15:11:29 +0800

    Update About page copywriting connects #54

--- vue-project/src/components/AboutPage.vue
+++ vue-project/src/components/AboutPage.vue
@@ -6,10 +6,35 @@
 
     <h1>About us</h1>
 
-    <p>
+<!--     <p>
       LGBTQ Glassdoor is an opensource project created by <a href="https://oursky.com">Oursky</a>, a software development agency headquarted in Hong Kong. You can check out our Github Repo <a href="https://github.com/oursky/">here.</a></p>
 
     <p>Get in touch if you are looking for a partner for web and mobile development and AI solutions for digital transformation!</p>
+ -->
+
+    <p>The LGBTQ Glassdoor is a side project developed by <a href="https://oursky.com">Oursky</a>, a software development agency with offices in Hong Kong and Taiwan. The LGBTQ Glassdoor aims to help LGBTQ individuals find inclusive workplaces in Hong Kong. The listings on the website are submitted by individuals, LGBTQ-identified and allies, who feel an organization (a company, NGO, or public agency) is LGBTQ-friendly.</p>
+
+    <p>We know that Hong Kong has a long way to go. Many people are not out in the workplace, sometimes even when a company has explicit same-sex or trans policies and benefits. At the same time, there may be companies that have no official policies, but have been supportive on individual cases when they are raised. Some organizations may have LGBTQ individuals and allies who want to be visible. The LGBTQ Glassdoor is meant to help capture these possibilities for LGBTQ-identified individuals to find positions in better work environments.</p>
+
+    <p>As the listings are submitted by individuals and teams can change over time, not all of the information may be updated. We recommend that individuals find out more information through a company's website and talking with staff. The LGBTQ Glassdoor as a directory is meant to be a starting point for individuals to find information and for listed organizations to work towards greater inclusion.</p>
+
+    <p>The LGBTQ Glassdoor is an opensource directory created with Oursky's opensource backend, <a href="https://skygear.io/">Skygear</a>. Anyone who would like to create a similar directory in their city or country can clone the LGBTQ Glassdoor repo. If you do, let us know!</p>
+
+    <p>You can also follow us on <a href="https://www.facebook.com/lgbtqglassdoor/">Facebook</a> and <a href="https://twitter.com/lgbtqglassdoor">Twitter</a>.</p>
+
+    <p>If you have any updates for listings, please contact us at hello[at]lgbtqglassdoor.com.</p>
+
+    <h4>About Oursky</h4>
+    <p>By 2018, nearly a decade after its founding, Oursky didn't have many official policies. Still, two out colleagues generally felt it was an inclusive workplace, but there was nowhere to share this information. We then wondered what other LGBTQ-friendly SMEs in Hong Kong were also under the radar. When we couldn't find any official list, we decided to create a platform to crowdsource information. The directory is created through the collaboration of both Oursky's LGBTQ-identified individuals and allies.</p>
+
+    <p>The Oursky team members who started this project in 2018 are Athena Lam, Kai Lam, Ten Tang, and May Yeung with invaluable contributions from Patrick Cheung, Jane Chung, Grace Lee, David Ng, and Queenie So.</p>
+
+    <h4>What counts as LGBTQ-friendly?</h4>
+    <p>LGBTQ-friendly for this project means that an LGBTQ-identified person generally feels comfortable to be themselves in the workplace, even if they are not necessarily out. We say “generally” because everyone has different comfort levels and also because workplaces can vary in their inclusiveness. For example, some places may have same-sex benefits, but lack trans-friendly policies.</p>
+
+    <p>In addition, we wanted LGBTQ individuals to find relevant information for their needs. For example, trans colleagues may want to find offices with gender-neutral washrooms while a same-sex couple may want to find a company with same-sex partner benefits.</p>
+
+    <hr>
 
     <h1>關於我們</h1>
 
