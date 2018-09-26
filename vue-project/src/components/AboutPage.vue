0349947bb84447fd5c1edf16e5381ab30a671fac
david90 <david.ng@oursky.com>
2018-09-26 14:31:50 +0800

    Add Chinese About page copywriting connects #56

--- vue-project/src/components/AboutPage.vue
+++ vue-project/src/components/AboutPage.vue
@@ -6,19 +6,13 @@
 
     <h1>About us</h1>
 
-<!--     <p>
-      LGBTQ Glassdoor is an opensource project created by <a href="https://oursky.com">Oursky</a>, a software development agency headquarted in Hong Kong. You can check out our Github Repo <a href="https://github.com/oursky/">here.</a></p>
-
-    <p>Get in touch if you are looking for a partner for web and mobile development and AI solutions for digital transformation!</p>
- -->
-
     <p>The LGBTQ Glassdoor is a side project developed by <a href="https://oursky.com">Oursky</a>, a software development agency with offices in Hong Kong and Taiwan. The LGBTQ Glassdoor aims to help LGBTQ individuals find inclusive workplaces in Hong Kong. The listings on the website are submitted by individuals, LGBTQ-identified and allies, who feel an organization (a company, NGO, or public agency) is LGBTQ-friendly.</p>
 
     <p>We know that Hong Kong has a long way to go. Many people are not out in the workplace, sometimes even when a company has explicit same-sex or trans policies and benefits. At the same time, there may be companies that have no official policies, but have been supportive on individual cases when they are raised. Some organizations may have LGBTQ individuals and allies who want to be visible. The LGBTQ Glassdoor is meant to help capture these possibilities for LGBTQ-identified individuals to find positions in better work environments.</p>
 
     <p>As the listings are submitted by individuals and teams can change over time, not all of the information may be updated. We recommend that individuals find out more information through a company's website and talking with staff. The LGBTQ Glassdoor as a directory is meant to be a starting point for individuals to find information and for listed organizations to work towards greater inclusion.</p>
 
-    <p>The LGBTQ Glassdoor is an opensource directory created with Oursky's opensource backend, <a href="https://skygear.io/">Skygear</a>. Anyone who would like to create a similar directory in their city or country can clone the LGBTQ Glassdoor repo. If you do, let us know!</p>
+    <p>The LGBTQ Glassdoor is an <a href="https://github.com/oursky/lgbtq-glassdoor">opensource</a> directory created with Oursky's opensource backend, <a href="https://skygear.io/">Skygear</a>. Anyone who would like to create a similar directory in their city or country can clone the LGBTQ Glassdoor repo. If you do, let us know!</p>
 
     <p>You can also follow us on <a href="https://www.facebook.com/lgbtqglassdoor/">Facebook</a> and <a href="https://twitter.com/lgbtqglassdoor">Twitter</a>.</p>
 
@@ -36,21 +30,44 @@
 
     <hr>
 
-    <h1>關於我們</h1>
+    <h1>關於</h1>
 
-    <p> LGBTQ Glassdoor是一個由<a href="https://oursky.com">Oursky</a>製作的開源項目。Oursky是一間在香港和台灣的軟件開發公司，我們活躍於開源社群，這是我們的開源項目的<a href="https://github.com/oursky/">列表</a>。</p>
+    <p>我們都知道，香港還有一段很長的路要走。有時候，即使企業有提供明確針對同性或跨性別的友善政策和權益，很多同志還是選擇不在工作場所公開出櫃。與此同時，有些公司可能沒有實施正式政策，但當一些個案被提出來討論的時候，公司是表示支持的。而在某些機構裡，可能有 LGBTQ 的個體或群體期待能被正視。 LGBTQ Glassdoor 希望能協助掌握這些可能性，讓 LGBTQ 人士能在更好的工作環境中求職或就職。</p>
 
-    <p>
-    如果你有軟件開發、數字化轉型的需要或在尋找人工智能解決方案，歡迎與我們聯絡。
-    </p>
+    <p>LGBTQ Glassdoor 旨在幫助 LGBTQ 同志在香港找尋更友善對待和接納他們的工作場所。網站上羅列的名單由個人、已確認多元性別身份的同志和同盟舉薦而組成，表明在該機構（公司、非牟利團體或政府機構）感受到性別友善對待。</p>
 
-    <h4>Follow us on Social Media</h4>
+    <p>由個人或團隊提交的名單可能會隨著時間變更，並非所有資訊都能及時更新。我們建議您在相關公司網頁獲得更多資訊，或直接與該公司的員工聯繫查詢。作為一個檢索目錄，LGBTQ Glassdoor 旨在提供一個起步點，幫助同志們找尋資訊，同時鼓勵名錄上的友善企業積極締造更接納、包容的工作環境。</p>
+
+    <p>LGBTQ Glassdoor 利用 Oursky 開源後台系統 <a href="https://skygear.io" target="_blank">Skygear</a> 打造，並獲 <a href="https://oursky.com" target="_blank">Oursky</a> 贊助為此項目免費提供網頁寄存服務及技術支援。</p>
+
+    <p>如果您有興趣為您的城市或國家建立一個類似的名錄，您可以考慮複製 LGBTQ Glassdoor 的<a href= "https://github.com/oursky/lgbtq-glassdoor" target="_blank">repo</a> 。假如您有這樣的意願，歡迎與我們聯絡！</p>
+
+    <p>同時也歡迎您在 <a href="https://facebook.com/lgbtqglassdoor" target="_blank">Facebook</a> 和 <a href="https://twitter.com/lgbtqglassdoor" target="_blank">Twitter</a> 追蹤我們。</p>
+
+    <h4>關於團隊</h4>
+    <p>LGBTQ Glassdoor 是由 Oursky 團隊成員建立的業餘項目，目前由以香港作為基地的團隊以義務形式獨立運作。</p>
+
+    <p>假如您有任何關於名錄的更新訊息，請透過 hello[at]lgbtqglassdoor.com 電郵聯繫我們。</p>
+
+    <h4>關於 Oursky</h4>
+    <p>由創立至今（2018 年）將近十年的歲月裡， Oursky 並沒有太多明確的 LGBTQ 友善政策。然而，兩名已出櫃的員工能普遍感受到這裡是一個包容多元性別的友善工作場所，但卻沒有渠道可以與人分享這項資訊。我們開始好奇其他存在於香港的 LGBTQ 友善企業是否為人所知，而當我們發現無法搜索到任何有關的官方名冊，我們決定創建一個平台來透過群眾募集相關資訊。這個平台由 Oursky 的 LGBTQ 員工還有支持多元性別的員工共同合作打造。</p>
+
+    <p>這個項目由 Oursky 團隊成員 Athena Lam、Kai Lam、Ten Tang 和 May Yeung 於 2018 年創立，並感謝 Patrick Cheung、Jane Chung、Grace Lee、David Ng 及 Queenie So 為此項目作出貢獻。Oursky 目前仍繼續以贊助網頁寄存方式支持這個獨立項目。</p>
+
+    <h4>怎樣定義為 LGBTQ 多元性別友善</h4>
+    <p>在這個項目裡，「LGBTQ 多元性別友善」意指自我認同為 LGBTQ 的人士在一個工作場所裡即使非必要出櫃，仍然對於表現自己普遍感到自在。我們提及「普遍」是因為每個人對於舒適程度的定義都不一樣，而各個工作場所對於多元性別的包容接納程度也不一樣。舉例來說，某些工作場所可能提供同性權益，但缺乏跨性別友善政策。</p>
+
+    <p>此外，我們期望 LGBTQ 人士能針對自己的需求找尋相關的資訊。例如，跨性別的同事可能希望尋找有提供跨性別友善洗手間的工作場所，而同性伴侶可能期待能找到職場提供同性伴侶福利。</p>
+
+    <hr>
+
+    <h1>Follow us on Social Media</h1>
 
     <a href="https://twitter.com/lgbtqglassdoor?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @lgbtqglassdoor</a>
 
     <div id="fb-root"></div>
 
-    <div class="fb-page" data-href="https://www.facebook.com/lgbtqglassdoor/" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/lgbtqglassdoor/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/lgbtqglassdoor/">LGBTQ Glassdoor</a></blockquote></div>
+    <div class="fb-page" data-href="https://www.facebook.com/lgbtqglassdoor/" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/lgbtqglassdoor/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/lgbtqglassdoor/">LGBTQ Glassdoor Facebook Page</a></blockquote></div>
 
   </b-container>
 </template>
@@ -58,4 +75,16 @@
 <script>
 export default {
 }
+</script>
+
+<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<script>
+   (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=262602944387701&autoLogAppEvents=1';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'))
 </script>
